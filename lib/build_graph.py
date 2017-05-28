e915b1bae89ec0767b87e3a203b2ee19b10ef3e3
Rob McDaniel <robmcdan@gmail.com>
2017-05-27 22:09:51 -0700

    updating copyright notice

--- lib/build_graph.py
+++ lib/build_graph.py
@@ -24,6 +24,9 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+
+Changes:
+May 2017: Rob Mcdaniel (Lingistic, LLC): cleanup and general bug fixes
 """
 __credits__ = ["Rob McDaniel"]
 __license__ = "ALv2"
